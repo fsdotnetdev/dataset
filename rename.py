60187bb02332096e154f65c296835ef11f648173
itcnida <natthasath.sak@nida.ac.th>
2019-06-22 17:13:12 +0700

    nsfw dataset

--- rename.py
+++ rename.py
@@ -1,10 +1,10 @@
 import os
 #path =  os.getcwd()
-path = "E:\Git\dataset\ikuta_erika"
+path = "./nsfw/ikuta_erika"
 filenames = os.listdir(path)
 i = 0
 for filename in filenames:
-    name = "ikuta_erika_" + str(i) + ".jpg"
+    name = "ikuta_erika." + str(i) + ".jpg"
     os.rename(os.path.join(path, filename), os.path.join(path, name))
     #os.rename(filename, "ikuta_erika_" + str(i) + ".jpg")
     i += 1

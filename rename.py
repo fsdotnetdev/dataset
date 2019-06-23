7656d1eec91d6515619a44f724ff9425f7ab7ca0
itcnida <natthasath.sak@nida.ac.th>
2019-06-23 20:24:38 +0700

    trashnet dataset

--- rename.py
+++ rename.py
@@ -1,10 +1,10 @@
 import os
 #path =  os.getcwd()
-path = "./nsfw/ikuta_erika"
+path = "./classification/waste/trashnet/trash"
 filenames = os.listdir(path)
 i = 0
 for filename in filenames:
-    name = "ikuta_erika." + str(i) + ".jpg"
+    name = "trash." + str(i) + ".jpg"
     os.rename(os.path.join(path, filename), os.path.join(path, name))
     #os.rename(filename, "ikuta_erika_" + str(i) + ".jpg")
     i += 1
